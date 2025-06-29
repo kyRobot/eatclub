f37016d706f52713b221632a112da7590ca58489
Kyle Milner <kylemilner@me.com>
2025-06-29 13:46:21 +1000

    bootstrap a spring web app

---
+++ src/main/java/com/kylemilner/eatclub/EatclubApplication.java
@@ -0,0 +1,13 @@
+package com.kylemilner.eatclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kylemilner/eatclub/EatclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kylemilner.eatclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatclubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
